9585bca0cf937ddc732b38f1e913524ebe8613f1
Peter Koch <peterept@gmail.com>
2017-06-23 11:21:26 +1000

    360 app in Unity 5.6.1p4

---
+++ Assets/VRToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRToggle : MonoBehaviour
+{
+
+	void Update() {
+
+		if (Input.GetMouseButtonDown (0)) {
+			ToggleVR ();
+		}
+	}
+
+	public void ToggleVR() {
+
+		if (VRSettings.loadedDeviceName == "cardboard") {
+			GoNormal ();
+		} else {
+			GoCardboard ();
+		}
+	}
+
+	public void GoNormal() {
+		StartCoroutine(LoadDevice("None"));
+	}
+
+	public void GoCardboard() {
+		StartCoroutine(LoadDevice("cardboard"));
+	}
+
+	IEnumerator LoadDevice(string newDevice)
+	{
+		VRSettings.LoadDeviceByName(newDevice);
+		yield return null;
+		VRSettings.enabled = true;
+	}
+}
